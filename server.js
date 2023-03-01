b438bf18be2bcd68feb40b9bab04160a6559d750
Yehanny Olivares <yolivares@gmail.com>
2023-02-28 23:20:51 -0500

    New custom server.js created

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+
+      switch (pathname) {
+        case "/about-us":
+          await app.render(req, res, "/about-us", query);
+          break;
+        case "/contact-us":
+          await app.render(req, res, "/contact-us", query);
+          break;
+        case "/portfolio":
+          await app.render(req, res, "/portfolio", query);
+          break;
+        default:
+          await handle(req, res, parsedUrl);
+          break;
+      }
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});

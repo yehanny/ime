a6f91b714538b10784f617ec0b681f542bb7d174
Yehanny Olivares <yolivares@gmail.com>
2023-03-02 00:21:54 -0500

    Change deprecated lightbox library, using React 18

--- .next/static/KZMXyRIrIJZ2xcMJHF8A8/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout-us","\u002Fcontact-us","\u002Fportfolio"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import * as React from "react";
+import "yet-another-react-lightbox/styles.css";
 import Head from "next/head";
 import CustomComponents from "../components/custom/Custom-components";
 import HeaderComponent from "../components/custom/sections/headercomponent";
@@ -19,6 +21,11 @@ export default function Home() {
       <HomeSlider />
       <Portfolio />
       <ClientSlider />
+      {/* <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={() => setOpen(true)}>
+        Open Lightbox
+      </button> */}
+
+      {/*  */}
       {/* <CustomComponents /> */}
       {/* <HeaderComponent /> */}
       {/* <CustomComponents /> */}

ceaa978734b0b7b9c77066047f53fcc261982b6f
Yehanny Olivares <yolivares@gmail.com>
2023-03-03 14:46:01 -0500

    Revert "Adding SMTP support to contact form"

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const { i18n } = require("./next-i18next.config");
-const { webpack } = require("./webpack.config");
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
-  webpack,
 };
 
 module.exports = nextConfig;

be8d68dadd78f4c9cea15586c469ab1503c8b4f1
Yehanny Olivares <yolivares@gmail.com>
2023-03-03 14:54:47 -0500

    Fixing conflicts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const { i18n } = require("./next-i18next.config");
+const { webpack } = require("./webpack.config");
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
+  webpack,
 };
 
 module.exports = nextConfig;
